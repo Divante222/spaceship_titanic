2964c6947c476b5f40f70f15d88bce5e17a6c531
Divante Parness <divante111@gmail.com>
2023-08-11 11:27:36 -0500

    only have predicting the test data left

--- wrangle.py
+++ wrangle.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+from sklearn.metrics import accuracy_score
 
 
 def prepare_data(df):
@@ -214,6 +215,7 @@ def modeling(X_train, X_test, y_train, y_test, means_list, chi2_list):
             p['model'] = models[number]
         new_df = pd.DataFrame(pd.DataFrame(grid.cv_results_['params']).sort_values('score', ascending=False))
         master_df = pd.concat([master_df, new_df])
+        
     return master_df
 
 
@@ -276,3 +278,170 @@ def percentages(df, y_train):
         contingency_table_percent = (contingency_table / total_observations) * 100
 
         print(contingency_table_percent)
+
+
+def comparison_of_means_visual(data):
+    num_cols = data.select_dtypes(include=['float64']).shape[1]
+    num_rows = (num_cols + 1) // 2  # Divide by 2 to arrange plots in a grid
+
+    fig, axes = plt.subplots(num_rows, 2, figsize=(12, 6 * num_rows))
+    axes = axes.flatten()  # Flatten the 2D array for easy indexing
+
+    for num, col in enumerate(data.select_dtypes(include=['float64']).columns):
+        sns.barplot(x='Transported', y=col, data=data, ax=axes[num], ci=None)
+        axes[num].set_xlabel('Transported')
+        axes[num].set_ylabel('Mean ' + col)
+        axes[num].set_title(col)
+        axes[num].set_xticks([0, 1])
+        axes[num].set_xticklabels(['Not Transported', 'Transported'])
+
+    plt.tight_layout()
+    plt.show()
+
+
+def get_percentages(X_train, y_train):
+    full_train = pd.concat([X_train, y_train], axis =1)
+    mars = pd.crosstab(full_train['HomePlanet_Mars'], full_train['Transported'], normalize='index') * 100
+    europa = pd.crosstab(full_train['HomePlanet_Europa'], full_train['Transported'], normalize='index') * 100
+    earth = pd.crosstab(full_train['HomePlanet_Earth'], full_train['Transported'], normalize='index') * 100
+
+    print('Percent chance of being transported if homeworld was mars', mars[1][1].round(2))
+    print('Percent chance of being transported if homeworld was europa', europa[1][1].round(2))
+    print('Percent chance of being transported if homeworld was earth', earth[1][1].round(2))
+
+
+def destination(X_train, y_train):
+    full_train = pd.concat([X_train, y_train], axis =1)
+    mars = pd.crosstab(full_train['Destination_55 Cancri e'], full_train['Transported'], normalize='index') * 100
+    europa = pd.crosstab(full_train['Destination_PSO J318.5-22'], full_train['Transported'], normalize='index') * 100
+    earth = pd.crosstab(full_train['Destination_TRAPPIST-1e'], full_train['Transported'], normalize='index') * 100
+
+    print("Percent chance of being transported if Destination_55 Cancri e", mars[1][1].round(2))
+    print('Percent chance of being transported if Destination_PSO J318.5-22', europa[1][1].round(2))
+    print('Percent chance of being transported if Destination_TRAPPIST-1e', earth[1][1].round(2))
+
+
+def transported_means(X_train, y_train):
+    full_train = pd.concat([X_train, y_train], axis = 1)
+    for col in full_train.select_dtypes('float64'):
+        if col != 'Age':
+            print(f'The average transported person spend on {col} was' ,full_train[col][full_train.Transported == 1].mean().round())
+            print(f'The average non-transported person spent on {col} was', full_train[col][full_train.Transported == 0].mean())
+            print()
+
+
+def modeling_new(X_train, X_test, y_train, y_test, means_list, chi2_list):
+    X_train = X_train[means_list + chi2_list]
+    X_test = X_test[means_list + chi2_list]
+
+
+    parameters_DTree = {
+        'max_depth':range(1,21),
+        'min_samples_leaf':range(1,11),
+        'criterion': ["gini", "entropy", "log_loss"]
+    }
+
+    parameters_rf = {'max_depth':range(1,21),
+                    "min_samples_leaf":range(1,21),
+                    "criterion": ['gini', 'entropy', 'log_loss']
+                    }
+
+    parameters_knn = {
+        'n_neighbors':range(1,21),
+        'weights':['uniform', 'distance'],
+        'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+        'leaf_size':range(1,31),
+        'p': range(1,5)
+
+    }
+
+    parameters_lr = {
+        'penalty':['l1', 'l2', 'elasticnet'],
+        'dual':[True, False], 
+        'C':range(1,21),
+
+    }
+
+
+    dt = DecisionTreeClassifier(random_state=123)
+    rf = RandomForestClassifier(random_state=123)
+    knn = KNeighborsClassifier()
+    lr = LogisticRegression(random_state = 123)
+
+    the_parameters = [parameters_rf]
+
+    models = ['DecisionTreeClassifier','RandomForestClassifier','KNeighborsClassifier','LogisticRegression']
+    master_df = pd.DataFrame()
+
+    for number, tree in enumerate([rf]):
+        grid = GridSearchCV(tree, the_parameters[number], cv=5)
+        grid.fit(X_train, y_train)
+
+        for p, score in zip(grid.cv_results_['params'], grid.cv_results_['mean_test_score'], ):
+            p['score'] = score
+            p['model'] = models[number]
+            
+        new_df = pd.DataFrame(pd.DataFrame(grid.cv_results_['params']).sort_values('score', ascending=False))
+
+        y_pred = grid.best_estimator_.predict(X_test)  # Predict on the test data
+        test_accuracy = grid.best_estimator_.score(X_test, y_test)  # Calculate test accuracy
+
+        new_df['Test_Accuracy'] = test_accuracy  # Add test accuracy to the DataFrame
+        master_df = pd.concat([master_df, new_df])
+
+    return master_df, grid.best_estimator_
+
+
+
+def modeling_small_scale(X_train, X_test, y_train, y_test, means_list, chi2_list):
+    X_train = X_train[means_list + chi2_list]
+    X_test = X_test[means_list + chi2_list]
+
+
+    parameters_DTree = {
+        'max_depth':range(1,21),
+        'min_samples_leaf':range(1,11),
+        'criterion': ["gini", "entropy", "log_loss"]
+    }
+
+    parameters_rf = {'max_depth':range(1,21),
+                    "min_samples_leaf":range(1,21),
+                    "criterion": ['gini', 'entropy', 'log_loss']
+                    }
+
+    parameters_knn = {
+        'n_neighbors':range(1,21),
+        'weights':['uniform', 'distance'],
+        'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+        'leaf_size':range(1,31),
+        'p': range(1,5)
+
+    }
+
+    parameters_lr = {
+        'penalty':['l1', 'l2', 'elasticnet'],
+        'dual':[True, False], 
+        'C':range(1,21),
+
+    }
+
+
+    dt = DecisionTreeClassifier(random_state=123)
+    rf = RandomForestClassifier(random_state=123)
+    knn = KNeighborsClassifier()
+    lr = LogisticRegression(random_state = 123)
+
+    the_parameters = [parameters_DTree]
+
+    models = ['DecisionTreeClassifier']
+    master_df = pd.DataFrame()
+
+    for number, tree in enumerate([dt]):
+        grid = GridSearchCV(tree, the_parameters[number], cv=5)
+        grid.fit(X_train, y_train)
+
+        for p, score in zip(grid.cv_results_['params'], grid.cv_results_['mean_test_score']):
+            p['score'] = score
+            p['model'] = models[number]
+        new_df = pd.DataFrame(pd.DataFrame(grid.cv_results_['params']).sort_values('score', ascending=False))
+        return new_df
